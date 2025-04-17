711be862d2b7281fc25ac782eebcedfec5688616
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2025-04-17 03:09:27 +0200

    Accepted 51.33% (Dynamic programming)

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 class Solution {
 public:
-    bool check(int i, int j, string& s)
-    {
-        while (j > i)
-        {
-            if(s[j] != s[i])
-                return false;
-            
-            j--;
-            i++;
-        }
-        return true;
-    }
-
     string longestPalindrome(string s) {
+        bool dp[1000][1000];
         int len = (int)s.size();
-        for(int i = len; i > 1; i--)
+        
+        int start = 0, end = 0;
+        for(int i = 0; i < len; i++)
+            dp[i][i] = true;
+        
+        for(int i = 0; i < len - 1; i++)
         {
-            for(int j = 0; j <= len - i; j++)
+            if(s[i] == s[i + 1]){
+                dp[i][i + 1] = true;
+                start = i;
+                end = i + 1;
+            }
+            else
+                dp[i][i + 1] = false;
+        }
+
+        for(int i = 2; i <= len; i++)
+        {
+            for(int j = 0; j < len - i; j++)
             {
-                if(check(j, j + i - 1, s))
-                    return s.substr(j, i);
+                if(s[j] == s[j + i] && dp[j + 1][j + i - 1]){
+                    dp[j][j + i] = true;
+                    start = j;
+                    end = j + i;
+                }else
+                    dp[j][j + i] = false;
             }
         }
-        return s.substr(0, 1);
+
+        return s.substr(start, end - start + 1);
     }
 };
-
-int main()
-{
-    Solution s;
-    cout << s.longestPalindrome("babad") << endl;
-}
